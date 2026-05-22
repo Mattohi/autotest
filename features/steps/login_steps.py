878084c880c9a070272c2b2f2c78eadf0fcc73dc
Joshua Alexander <josh@MacBook-Air-Joshua.local>
2026-05-22 22:39:42 +0700

    fix safari login automation

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,15 +1,15 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @given('User membuka halaman login')
 def step_impl(context):
 
     context.driver = webdriver.Safari()
-
     context.driver.maximize_window()
-
     context.driver.get(
         "https://the-internet.herokuapp.com/login"
     )
@@ -45,24 +45,28 @@ def step_impl(context):
 @then('User berhasil login')
 def step_impl(context):
 
-    text = context.driver.find_element(
-        By.ID,
-        "flash"
-    ).text
+    flash = WebDriverWait(
+        context.driver,10
+    ).until(
+        EC.presence_of_element_located(
+            (By.ID,"flash")
+        )
+    )
 
-    assert "You logged into a secure area!" in text
-
+    assert "You logged into a secure area!" in flash.text
     context.driver.quit()
 
 
 @then('User gagal login')
 def step_impl(context):
 
-    text = context.driver.find_element(
-        By.ID,
-        "flash"
-    ).text
+    flash = WebDriverWait(
+        context.driver,10
+    ).until(
+        EC.presence_of_element_located(
+            (By.ID,"flash")
+        )
+    )
 
-    assert "Your password is invalid!" in text
-
+    assert "Your password is invalid!" in flash.text
     context.driver.quit()
